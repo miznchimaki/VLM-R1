c858e9f9a9cc57ae6d5ea114ed4d5554d9c13002
miznchimaki <miznchimaki@gmail.com>
2025-07-23 16:20:19 +0800

    name modification

--- src/open-r1-multimodal/src/open_r1/vlm_modules/__init__.py
+++ src/open-r1-multimodal/src/open_r1/vlm_modules/__init__.py
@@ -1,6 +1,7 @@
 from .vlm_module import VLMBaseModule
 from .qwen_module import Qwen2VLModule
 from .internvl_module import InvernVLModule
-from .glm4v_module import GLM4vModule
+from .glm4v_module import Glm4vModule
 
-__all__ = ["VLMBaseModule", "Qwen2VLModule", "InvernVLModule", "GLM4vModule"]
+
+__all__ = ["VLMBaseModule", "Qwen2VLModule", "InvernVLModule", "Glm4vModule"]
