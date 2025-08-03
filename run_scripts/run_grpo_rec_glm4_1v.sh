4050452822cf0f9f252ea5b52c762804e4aa1267
miznchimaki <miznchimaki@gmail.com>
2025-08-03 21:57:58 +0800

    bash script for GLM-4.1V training completed

--- run_scripts/run_grpo_rec_glm4_1v.sh
+++ run_scripts/run_grpo_rec_glm4_1v.sh
@@ -1,57 +1,81 @@
+#!/usr/bin/bash
+
+
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-export REPO_HOME="${PROJECT_ROOT}"
-echo "REPO_HOME: $REPO_HOME"
-# Change the data_paths and image_folders to your own data
-data_paths="/training/shz/dataset/vlm-r1/rec_jsonsl_train/refcoco_train.jsonl:/training/shz/dataset/vlm-r1/rec_jsonsl_train/refcocop_train.jsonl:/training/shz/dataset/vlm-r1/rec_jsonsl_train/refcocog_train.jsonl" 
-image_folders="/training/shz/dataset/coco:/training/shz/dataset/coco:/training/shz/dataset/coco"
-model_path="/training/models/Qwen2.5-VL-3B-Instruct"
-is_reward_customized_from_vlm_module=True
-echo "data_paths: $data_paths"
-echo "image_folders: $image_folders"
+nproc_per_node=${1:-"8"}
+nnodes=${2:-"1"}
+node_rank=${3:-"0"}
+master_addr=${4:-"127.0.0.1"}
+master_port=${5:-"15469"}
 
-export EXP_NAME="Qwen2.5-VL-3B-Instruct-rec" # TODO: change this to your own experiment name
-TASK_TYPE="rec"
-cd ${REPO_HOME}/src/open-r1-multimodal
+default_data_paths="${HOME}/datasets/rec_jsonsl_train/refcoco_train.jsonl:"
+default_data_paths+="${HOME}/datasets/rec_jsonsl_train/refcocop_train.jsonl:"
+default_data_paths+="${HOME}/datasets/rec_jsonsl_train/refcocog_train.jsonl"
+data_paths=${6:-"${default_data_paths}"}
 
-# export DEBUG_MODE="true" # Enable Debug if you want to see the rollout of model during RL
-# create the run directory and log file
-mkdir -p ${REPO_HOME}/runs/${EXP_NAME}/log
-export LOG_PATH="${REPO_HOME}/runs/${EXP_NAME}/log/debug_log.$(date +%Y-%m-%d-%H-%M-%S).txt"
-# MAX_STEPS=1200 # TODO: change this to your own max steps
+default_image_folders="${HOME}/datasets/coco:"
+default_image_folders+="${HOME}/datasets/coco:"
+default_image_folders+="${HOME}/datasets/coco"
+image_folders=${7:-"${default_image_folders}"}
+
+model_path=${8:-"${HOME}/ckpts/GLM-4.1V-9B-Thinking"}
+is_reward_customized_from_vlm_module=${9:-"True"}
+exp_name=${10:-"GLM-4.1V-9B-Thinking-baseline"}
+task_type=${11:-"rec"}
+task_type_for_format_reward=${12:-"rec"}
+max_steps=${13:-"1200"}
+debug_mode=${14:-"false"}
+
+cd ${PROJECT_ROOT}/src/open-r1-multimodal
+export DEBUG_MODE=${debug_mode}  # Enable Debug if you want to see the rollout of model during RL
+output_dir=${HOME}/outputs/${exp_name}
+if [ -d ${output_dir} ]; then
+  rm -rf ${output_dir}
+fi
+mkdir -p ${output_dir}
+export log_path=${output_dir}/output.log
 
 
-# export WANDB_DISABLED=true
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6
-torchrun --nproc_per_node="8" \
-    --nnodes="1" \
-    --node_rank="0" \
-    --master_addr="127.0.0.1" \
-    --master_port="12349" \
-  src/open_r1/grpo_jsonl.py \
-    --use_vllm False \
-    --output_dir ${REPO_HOME}/checkpoints/rl/${EXP_NAME} \
-    --resume_from_checkpoint True \
-    --model_name_or_path $model_path \
-    --data_file_paths $data_paths \
-    --image_folders $image_folders \
-    --is_reward_customized_from_vlm_module $is_reward_customized_from_vlm_module \
-    --task_type $TASK_TYPE \
-    --per_device_train_batch_size 8 \
-    --gradient_accumulation_steps 2 \
-    --gradient_checkpointing true \
-    --logging_steps 1 \
-    --num_train_epochs 2 \
-    --bf16 \
-    --attn_implementation flash_attention_2 \
-    --run_name ${EXP_NAME} \
-    --data_seed 42 \
-    --save_steps 100 \
-    --num_generations 8 \
-    --max_completion_length 2048 \
-    --reward_funcs accuracy format \
-    --beta 0.04 \
-    --report_to wandb \
-    --dataset-name this_is_not_used \
-    --deepspeed ${REPO_HOME}/src/open-r1-multimodal/local_scripts/zero3.json \
+log_func() {
+  "$@" 2>&1 | tee --append ${log_path}
+}
 
-echo "Training completed for ${EXP_NAME}"
+
+echo "data paths for training: ${data_paths}" 2>&1 | tee ${log_path}
+log_func echo "image folders for training: ${image_folders}"
+log_func echo "start GRPO traning at `date +%Y-%m-%d-%H-%M-%S`"
+
+log_func torchrun --nproc_per_node=${nproc_per_node} \
+             --nnodes=${nnodes} \
+             --node_rank=${node_rank} \
+             --master_addr=${master_addr} \
+             --master_port=${master_port} \
+           src/open_r1/grpo_jsonl.py \
+             --use_vllm False \
+             --output_dir ${output_dir} \
+             --resume_from_checkpoint True \
+             --model_name_or_path ${model_path} \
+             --data_file_paths ${data_paths} \
+             --image_folders ${image_folders} \
+             --is_reward_customized_from_vlm_module ${is_reward_customized_from_vlm_module} \
+             --task_type ${task_type} \
+             --task_type_for_format_reward ${task_type_for_format_reward} \
+             --per_device_train_batch_size 8 \
+             --gradient_accumulation_steps 2 \
+             --gradient_checkpointing true \
+             --logging_steps 1 \
+             --max_steps ${max_steps} \
+             --num_train_epochs 2 \
+             --bf16 \
+             --attn_implementation flash_attention_2 \
+             --run_name ${exp_name} \
+             --data_seed 42 \
+             --save_steps 100 \
+             --num_generations 8 \
+             --max_completion_length 2048 \
+             --reward_funcs accuracy format \
+             --beta 0.04 \
+             --report_to wandb \
+             --deepspeed ${PROJECT_ROOT}/src/open-r1-multimodal/local_scripts/zero3.json
+
+log_func echo "end GRPO traning at `date +%Y-%m-%d-%H-%M-%S`"
